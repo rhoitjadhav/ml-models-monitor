86358cfaa88f185a4bd4c8ac5cd128d4bd8f1c83
rhoitjadhav <jadhavrohit497@gmail.com>
2021-02-14 20:11:14 +0530

    configuration file

---
+++ ml_monitor/src/config.py
@@ -0,0 +1,7 @@
+import os
+
+BASE_DIR = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+SERVER_NAME = 'localhost:5001'
+ML_MODELS_SERVER_NAME = 'localhost:5002'
+ENV = 'development'
+DEBUG = True
