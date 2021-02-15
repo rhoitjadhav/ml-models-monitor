e54ecc8e93348f431b6c08b314900c5b7f4056fa
rhoitjadhav <jadhavrohit497@gmail.com>
2021-02-15 19:32:13 +0530

    executes ml models

---
+++ ml_models/run_model.py
@@ -0,0 +1,8 @@
+import sys
+from src.housing_price import model as model1
+from src.payment_fraud import model as model2
+
+
+if __name__ == '__main__':
+        model1.main()
+        model2.main()
